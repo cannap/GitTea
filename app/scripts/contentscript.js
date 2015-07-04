171cea10db015e0d47724a41d3ac47f83032844d
cannap <sh4fty@gmail.com>
2015-07-04 19:34:46 +0200

    fixes

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,55 +1,60 @@
 'use strict';
 var Tea;
 Tea = {
-    currentLang: null,
+    isObserving: false,
+
     languages: [
         'js',
         'coffee',
         'html',
         'jade'
     ],
-    possibleCompiles: [
-        ['js', 'coffee'],
-        ['coffee', 'js'],
-        ['html', 'jade'],
-        ['jade', 'html']
-    ],
+
+    possibleCompiles: {
+        'js': 'coffee',
+        'coffee': 'js',
+        'html': 'jade',
+        'jade': 'html'
+    },
 
     init: function () {
         var language = $('strong.final-path').text();
         language = language.split('.')[1];
+        //Check if not Running or and it has no language then we can determine this is not a Singlefile
+        if (!language && !Tea.isObserving) {
+            Tea.detectDomChange();
+        }
+
         //Check if language is supported
-       if(!language) return;
         this.languages.forEach(function (lang) {
             if (language === lang) {
+                console.log(lang);
                 Tea.currentLang = lang;
-                Tea.compile(lang);
-                return;
+                return true;
             }
         });
     },
 
-
-    compile: function (lang) {
-
-    },
-
     detectDomChange: function () {
 
-        Tea.init();
+        Tea.isObserving = true;
+
         var target = document.querySelector('#js-repo-pjax-container');
         var observer = new MutationObserver(function (mutation) {
-            console.log(mutation);
             Tea.init();
         });
-        var config = {attributes: false, childList: true, characterData: false};
+        var config = {
+            attributes: false,
+            childList: true,
+            characterData: false
+        };
         observer.observe(target, config);
     }
-    
+
 };
 
 
 //Fire
-Tea.detectDomChange();
+Tea.init();
 
 
