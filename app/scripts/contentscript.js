ca4676fc07817823e19974071bfa0f3cb154b2a3
cannap <sh4fty@gmail.com>
2015-07-04 15:09:48 +0200

    added detect language from direct

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,44 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+var Tea = {
+
+    currentLang: null,
+
+    languages: [
+        'js',
+        'coffee',
+        'html',
+        'jade'
+    ],
+
+    possibleCompiles: [
+        ['js', 'coffee'],
+        ['coffee', 'js'],
+        ['html', 'jade'],
+        ['jade', 'html']
+    ],
+
+
+    init: function () {
+        //Detect Language
+        var language = $('strong.final-path').text();
+        language = language.split('.')[1];
+        //Check if language is supported
+        this.languages.forEach(function (lang) {
+            if (language === lang) {
+                this.currentLang = lang;
+                return;
+            }
+        });
+    }
+};
+
+
+//Fire
+Tea.init();
+
+
+
+
+
+
